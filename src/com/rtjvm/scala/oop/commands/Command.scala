98d25014081c8c050ef42352eb23bc1c28825c89
Marco Arnone <m.arnone@outlook.com>
2019-01-12 16:24:25 +0100

    improvment

--- src/com/rtjvm/scala/oop/commands/Command.scala
+++ src/com/rtjvm/scala/oop/commands/Command.scala
@@ -2,10 +2,10 @@ package com.rtjvm.scala.oop.commands
 
 import com.rtjvm.scala.oop.filesystem.State
 
-// TODO extends (State => State) and eliminate apply method
-trait Command {
+trait Command extends (State => State)
+/* trait Command {
   def apply(state: State): State
-}
+} */
 
 object Command {
   val MKDIR = "mkdir"
@@ -28,30 +28,32 @@ object Command {
 
   def from(input: String): Command = {
     val tokens: Array[String] = input.split(" ")
-
-    // TODO use pattern match
+    
     if(input.isEmpty || tokens.isEmpty) emptyCommand
-    else if(MKDIR.equals(tokens(0))) {
-      if(tokens.length < 2) incompleteCommand(MKDIR)
-      else new Mkdir(tokens(1))
-    } else if(LS.equals(tokens(0))) {
-      new Ls
-    } else if(PWD.equals(tokens(0))) {
-      new Pwd
-    } else if(TOUCH.equals(tokens(0))) {
-      new Touch(tokens(1))
-    } else if(CD.equals(tokens(0))) {
-      if(tokens.length < 2) incompleteCommand(MKDIR)
-      else new Cd(tokens(1))
-    } else if(RM.equals(tokens(0))) {
-      if(tokens.length < 2) incompleteCommand(RM)
-      else new Rm(tokens(1))
-    } else if(ECHO.equals(tokens(0))) {
-      if(tokens.length < 2) incompleteCommand(ECHO)
-      else new Echo(tokens.tail)
-    } else if(CAT.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(CAT)
-      else new Cat(tokens(1))
-    } else new UnknownCommand
+    else tokens(0) match {
+      case MKDIR =>
+        if(tokens.length < 2) incompleteCommand(MKDIR)
+        else new Mkdir(tokens(1))
+      case LS =>
+        new Ls
+      case PWD =>
+        new Pwd
+      case TOUCH =>
+        new Touch(tokens(1))
+      case CD =>
+        if(tokens.length < 2) incompleteCommand(CD)
+        else new Cd(tokens(1))
+      case RM =>
+        if(tokens.length < 2) incompleteCommand(RM)
+        else new Rm(tokens(1))
+      case ECHO =>
+        if(tokens.length < 2) incompleteCommand(ECHO)
+        else new Echo(tokens.tail)
+      case CAT =>
+        if (tokens.length < 2) incompleteCommand(CAT)
+        else new Cat(tokens(1))
+      case _ =>
+        new UnknownCommand
+    }
   }
 }
